68d0b7662cf3d45fc8a62b26fda2031f44f7bc1a
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-08-02 16:15:11 +0530

    Trial an error

--- src/Components/Deals.js
+++ src/Components/Deals.js
@@ -142,7 +142,7 @@ export function Deals(){
                                     const isInCart = cartedProducts.includes(item._id);
                                     const rel_path = item.product_image.split("\\");
                                     const len = item.product_image.split("/").length;
-                                    // console.log("path is",rel_path[len-1]);
+                                    console.log("path is",rel_path[len-1]);
                                     return(
                                         <div id="home-product-card" key={id}>
                                             <div id="part-1">
